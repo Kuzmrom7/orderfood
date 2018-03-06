58d4f781b79d99b405b351eb438a2ef5346d3d49
Kuzmrom7 <kuzmrom7@gmail.com>
2018-03-06 18:38:31 +0300

    refctot upload

--- src/components/Card/CardDishCreate.js
+++ src/components/Card/CardDishCreate.js
@@ -184,7 +184,7 @@ class CardDishCreate extends Component {
                   {this.state.uploadedFileCloudinaryUrl === '' ? null :
                     <div>
                       <p>{this.state.uploadedFile.name}</p>
-                      <img src={this.state.uploadedFileCloudinaryUrl}/>
+                      <img src={this.state.uploadedFileCloudinaryUrl} alt=""/>
                     </div>}
                 </div>
                 <div className="col-md-2">
