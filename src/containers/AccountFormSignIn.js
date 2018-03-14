185d864d30b73962602853d7e2e10070ac5baa4a
Kuzmrom7 <kuzmrom7@gmail.com>
2018-03-14 19:58:56 +0300

    backgroundphoto for signin

--- src/containers/AccountFormSignIn.js
+++ src/containers/AccountFormSignIn.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import {Link} from 'react-router-dom';
 import logo from '../assets/img/reactlogo.png';
+import {Paper} from "material-ui";
 
 class AccountFormSignIn extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
 
     let data = this.state.data;
-    console.log("DATA", this.state.data)
 
     this.props.submit(data.username, data.password)
       .then(() => console.log())
@@ -40,32 +40,34 @@ class AccountFormSignIn extends Component {
 
   render() {
     return (
-      <form className="form-signin" onSubmit={this.handleSubmit}>
-        <div className="container">
-          <a className="simple-text logo-normal">
-            <h1 className="form-signin-heading"><img src={logo} width="60" alt="logo_image"/>rderFood</h1>
-          </a>
-        </div>
-        <h2 className="form-signin-heading">Пожалуйста войдите</h2>
-        <input type="username" className="form-control"
-               placeholder="Username"
-               onChange={this.handleChangeUsername}
-               value={this.state.username}
-        />
-        <br/>
-        <input type="password" className="form-control"
-               placeholder="Password"
-               onChange={this.handleChangePassword}
-               value={this.state.password}
-        />
-        <br/>
-        <button className="btn btn-lg btn-primary" type="submit">Войти</button>
-        <Link to={"/signup"}>
-          <div className="pull-right">
-            <button className="btn btn-lg btn-success">Регистрация</button>
+      <Paper zDepth={4} className="p-2">
+        <form className="form-signin m-4 " onSubmit={this.handleSubmit}>
+          <div className="container">
+            <a className="simple-text logo-normal">
+              <h1 className="form-signin-heading"><img src={logo} width="60" alt="logo_image"/>rderFood</h1>
+            </a>
           </div>
-        </Link>
-      </form>
+          <h3 className="form-signin-heading">Пожалуйста войдите</h3>
+          <input type="username" className="form-control"
+                 placeholder="Username"
+                 onChange={this.handleChangeUsername}
+                 value={this.state.username}
+          />
+          <br/>
+          <input type="password" className="form-control"
+                 placeholder="Password"
+                 onChange={this.handleChangePassword}
+                 value={this.state.password}
+          />
+          <br/>
+          <button className="btn btn-lg btn-primary" type="submit">Войти</button>
+          <Link to={"/signup"}>
+            <div className="pull-right">
+              <button className="btn btn-lg btn-success">Регистрация</button>
+            </div>
+          </Link>
+        </form>
+      </Paper>
     );
   }
 }
--- src/layout/PageSignIn.js
+++ src/layout/PageSignIn.js
@@ -2,7 +2,21 @@ import React, {Component} from 'react';
 import Session from "../actions/session";
 import AccountFormSignIn from "../containers/AccountFormSignIn";
 import {connect} from "react-redux";
+import {blue100, blue700, blueGrey500} from "material-ui/styles/colors";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blueGrey500,
+    primary2Color: blue700,
+    primary3Color: blue100,
+  },
+}, {
+  avatar: {
+    borderColor: null,
+  },
+});
 
 class PageSignIn extends Component {
 
@@ -22,13 +36,17 @@ class PageSignIn extends Component {
   render() {
 
     return (
-      <div className="container">
-        <div className="col-md-4">
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={{backgroundImage: `url('./2.jpg')`, height: '100vh', overflow : 'hidden',backgroundSize: 'cover'}}>
+          <div className="container">
+            <div className="col-md-4">
+            </div>
+            <div className="col-md-4 col-sm-12 mt-4">
+              <AccountFormSignIn submit={this.handlerSessionCreate}/>
+            </div>
+          </div>
         </div>
-        <div className="col-md-4">
-          <AccountFormSignIn submit={this.handlerSessionCreate}/>
-        </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
